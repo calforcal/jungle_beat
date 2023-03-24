f0f7f621490f8796fbfb2ce008e5db0161bd46a8
Michael Callahan <mcalla242@gmail.com>
2023-03-23 18:49:09 -0600

    Add Node class and spec file for testing

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,6 @@
 class Node
-  def initialize
-    
+  attr_reader :data
+  def initialize(data)
+    @data = data
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,7 +2,12 @@ require "./node"
 
 RSpec.describe Node do
   it "initializes" do
-    node = Node.new
+    node = Node.new("plop")
     expect(node).to be_an_instance_of(Node)
+  end
+
+  it "accepts data" do
+    node = Node.new("plop")
+    expect(node.data).to eq("plop")
   end
 end
