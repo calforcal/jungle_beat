4e6829c7914e810c9e3ee88fb7fca70d60eae02e
Michael Callahan <mcalla242@gmail.com>
2023-03-25 09:04:47 -0600

    Add runner file for JungleBeat

---
+++ jungle_beat_runner.rb
@@ -0,0 +1,8 @@
+require "./lib/jungle_beat"
+require "./lib/node"
+require "./lib/linked_list" 
+
+jb = JungleBeat.new
+jb.append("Bee boo boo bop Bee boo boo bee bop Bop bee boo boo bee bop boo bop Bee boo boo bee boo bop")
+
+jb.play
